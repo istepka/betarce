0bddbf784437ece4eb0b36476564a2d22b4e2542
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-02-20 01:01:04 +0100

    Add notebook with torch MLPs

--- src/create_data_examples.py
+++ src/create_data_examples.py
@@ -323,8 +323,8 @@ class DatasetPreprocessor:
         '''
         X_train = self.X_train.values
         X_test = self.X_test.values
-        y_train = self.y_train.values
-        y_test = self.y_test.values
+        y_train = np.array(self.y_train)
+        y_test =  np.array(self.y_test)
         return X_train, X_test, y_train, y_test
 
 
