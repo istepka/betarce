e1201d299248af00bfb4033d582cd8419bc49076
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-03-29 18:15:44 +0100

    Add configs for dt

---
+++ bash_scripts/dt/run_robx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p obl
+#SBATCH -w obl-1
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time=144:00:00               # Time limit hrs:min:sec
+#SBATCH --output=logs/output_%j.out
+#SBATCH --error=logs/error_%j.err
+
+CONFIG_TO_USE="configs/configv2_robx.yml"
+
+source /home/inf148179/anaconda3/bin/activate
+conda activate robustcf
+
+echo "Running experiment with config: ${CONFIG_TO_USE}"
+
+python ./src/experimentsv2.py --config $CONFIG_TO_USE 
+
+
+echo "Job done"
