b73ec45841dcad82416926e9cd839b06ef476fc7
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-04-06 14:06:54 +0000

    Add new scripts

---
+++ bash_scripts/hope/k64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p hgx
+#SBATCH -w hgx2
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time=144:00:00               # Time limit hrs:min:sec
+#SBATCH --output=logs/output_%j.out
+#SBATCH --error=logs/error_%j.err
+
+EXPERIMENT="k64"
+CONFIG_TO_USE="configs/hope/configv2_${EXPERIMENT}.yml"
+
+source /home/inf148179/anaconda3/bin/activate
+conda activate robustcf
+
+echo "Running experiment with config: ${CONFIG_TO_USE}"
+
+python ./src/experimentsv2.py --config $CONFIG_TO_USE 
+
+
+echo "Job done"
---
+++ bash_scripts/lowk/run_bc_e1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p pmem
+#SBATCH -w pmem-6
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time=144:00:00               # Time limit hrs:min:sec
+#SBATCH --output=logs/output_%j.out
+#SBATCH --error=logs/error_%j.err
+
+DATASET="breast"
+EXPERIMENT="lowk"
+CONFIG_TO_USE="configs/lowk/configv2_${DATASET}_${EXPERIMENT}.yml"
+
+source /home/inf148179/anaconda3/bin/activate
+conda activate robustcf
+
+echo "Running experiment with config: ${CONFIG_TO_USE}"
+
+python ./src/experimentsv2.py --config $CONFIG_TO_USE 
+
+
+echo "Job done"
