4a1d522ff4c324c096d7927a82ee646cb99df01a
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-04-12 00:43:41 +0200

    Add bunch of stuifgf

--- max_delta_analysis_plot.py
+++ max_delta_analysis_plot.py
@@ -1,4 +1,6 @@
 import scipy.stats as stats
+import pandas as pd
+
 confidence = 0.95
 a = 48
 b = 1
@@ -12,7 +14,7 @@ print(stats.beta.ppf(0.025, a, b))
 import matplotlib.pyplot as plt
 import numpy as np
 
-k = [1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60]
+k = [1, 2] + [i for i in range(4, 128, 4)]
 conf = [0.7,  0.8,  0.9, 0.95, 0.99]
 
 maxdeltas = []
@@ -34,7 +36,8 @@ ax.set_xlabel('Number of estimators k')
 ax.set_ylabel('Max achievable delta')
 ax.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1])
 
-plt.grid(axis='y')
+# plt.grid(axis='y')
+plt.grid()
 plt.title('Max achievable delta-Robustness for different alpha-confidence levels')
 plt.legend()
 plt.tight_layout()
@@ -42,3 +45,26 @@ plt.tight_layout()
 plt.savefig('images/betarob/max_delta_analysis.png', dpi=300, bbox_inches='tight')
 
 plt.show()
+
+
+
+k = [1, 2] + [i for i in range(4, 128, 4)]
+conf = [0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+
+maxdeltas = []
+
+for c in conf:
+    deltas = []
+    for i in k:
+        lb, rb = stats.beta.interval(c, 0.5 + i, 0.5)
+        deltas.append(lb)
+    maxdeltas.append(deltas)
+
+# Create also latex table for the max deltas for the different confidence levels
+df = pd.DataFrame(maxdeltas, index=conf, columns=k)
+# Index name
+df.index.name = 'Confidence'
+# Create latex table
+df.T.to_latex('images/betarob/max_delta_analysis.tex', float_format="%.3f", label='tab:maxdelta', caption='Max achievable delta-Robustness for different alpha-confidence levels')
+
+print(df)
