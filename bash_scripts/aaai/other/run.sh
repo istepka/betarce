b0bd0fc122f894f5f694161a98eeee97581132ff
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-07-18 12:06:39 -0400

    Fix config for exp

--- bash_scripts/aaai/other/run.sh
+++
@@ -1,4 +0,0 @@
-python ./src/experimentsv3.py --config ./bash_scripts/aaai/other/face.yml
-python ./src/experimentsv3.py --config ./bash_scripts/aaai/other/rbr.yml 
-python ./src/experimentsv3.py --config ./bash_scripts/aaai/other/roar.yml 
-python ./src/experimentsv3.py --config ./bash_scripts/aaai/other/gs.yml 
--- bash_scripts/aaai/run_just_base.sh
+++ bash_scripts/aaai/run_just_base.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=betaRCE
+#SBATCH --job-name=BRCE_base
 #SBATCH --output=slurm_logs/aaai2/nn_exp_%A_%a.out
 #SBATCH --error=slurm_logs/aaai2/nn_exp_%A_%a.err
 #SBATCH --array=0-15
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=64G
 #SBATCH --time=168:00:00
 #SBATCH --partition=obl
-#SBATCH --nodelist=obl2
+#SBATCH --nodelist=pmem
 
 # Array of configuration files
 configs=(
-    "./bash_scripts/aaai/dice/nn_breast.yml"
-    "./bash_scripts/aaai/dice/nn_wine.yml"
-    "./bash_scripts/aaai/dice/nn_fico.yml"
-    "./bash_scripts/aaai/dice/nn_diabetes.yml"
-    "./bash_scripts/aaai/face/nn_breast.yml"
-    "./bash_scripts/aaai/face/nn_wine.yml"
-    "./bash_scripts/aaai/face/nn_fico.yml"
-    "./bash_scripts/aaai/face/nn_diabetes.yml"
-    "./bash_scripts/aaai/gs/nn_breast.yml"
-    "./bash_scripts/aaai/gs/nn_wine.yml"
-    "./bash_scripts/aaai/gs/nn_fico.yml"
-    "./bash_scripts/aaai/gs/nn_diabetes.yml"
     "./bash_scripts/aaai/other/face.yml"
     "./bash_scripts/aaai/other/dice.yml"
     "./bash_scripts/aaai/other/rbr.yml"
