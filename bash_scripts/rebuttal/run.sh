c3e449dfcf30becaaaf6dab4d18d6091ede1d25e
Ignacy Stępka <ignacy.stepka@gmail.com>
2024-07-26 21:28:58 -0400

    Move things around

--- bash_scripts/rebuttal/run.sh
+++
@@ -1,3 +0,0 @@
-python ./src/experimentsv3.py --config ./bash_scripts/rebuttal/face.yml 
-python ./src/experimentsv3.py --config ./bash_scripts/rebuttal/roar.yml 
-python ./src/experimentsv3.py --config ./bash_scripts/rebuttal/rbr.yml 
